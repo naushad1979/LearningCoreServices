6da39fa462d4cd4ecbf1b8003b7ff32ec39c0a2e
NaushadIvyTech <67053283+NaushadIvyTech@users.noreply.github.com>
2020-11-15 15:17:41 +0530

    GetWeatherDetails service endpoint added

--- Services/Basket/Basket.Api/Controllers/WeatherForecastController.cs
+++ Services/Basket/Basket.Api/Controllers/WeatherForecastController.cs
@@ -35,5 +35,17 @@ namespace Basket.Api.Controllers
             })
             .ToArray();
         }
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetWeatherDetails()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
